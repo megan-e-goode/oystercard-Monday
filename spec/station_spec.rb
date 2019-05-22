1c136d8bbd98539c7bec7e7c16cb6e7bb267403a
Megan Goode <megan.e.goode@hotmail.com>
2019-05-22 14:52:47 +0100

    Add zone and name to station class.

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,13 +1,17 @@
 require 'station'
 
 describe Station do
-  let(:station) { Station.new }
+  let(:station) { Station.new("Manor House", 2) }
 
   it 'can create a station' do
     expect(station).to be_an_instance_of(Station)
   end
 
   it 'can store the station zone' do
+    expect(station.zone).to eq(2)
+  end
 
+  it 'can store the station name' do
+    expect(station.name).to eq("Manor House")
   end
 end
