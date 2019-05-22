ec14d7cea06d577eef69401447fca32ace7a1267
Megan Goode <megan.e.goode@hotmail.com>
2019-05-22 14:39:42 +0100

    Add Station class and tests

---
+++ spec/station_spec.rb
@@ -0,0 +1,13 @@
+require 'station'
+
+describe Station do
+  let(:station) { Station.new }
+
+  it 'can create a station' do
+    expect(station).to be_an_instance_of(Station)
+  end
+
+  it 'can store the station zone' do
+
+  end
+end
