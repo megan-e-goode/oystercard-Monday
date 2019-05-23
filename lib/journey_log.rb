5c262e87992419d10fd092366228b43e16926c0f
Megan Goode <megan.e.goode@hotmail.com>
2019-05-23 15:39:57 +0100

    Add start_journey in joureny_log class

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -3,4 +3,8 @@ class JourneyLog
   def initialize(journey_class=Journey)
     @journey = journey_class.new
   end
+
+  def start_journey
+    @journey.begin
+  end
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -2,8 +2,16 @@ require './lib/journey_log.rb'
 require './lib/journey.rb'
 
 describe JourneyLog do
+let(:journeylog) { JourneyLog.new(journey_double_class) }
+let(:journey_double_class) { double(:journey, :new => journey) }
+let(:journey) { double(:journey, :begin => true) }
+
   it 'initializes with a class of Journey' do
-    journeylog = JourneyLog.new
-    expect(journeylog.journey).to be_an_instance_of(Journey)
+    expect(journeylog.journey).to eq(journey)
+  end
+
+  it 'can tell journey to begin' do
+    expect(journey).to receive(:begin)
+    journeylog.start_journey
   end
 end
